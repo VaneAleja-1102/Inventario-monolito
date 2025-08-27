eba2dc8ac4c2d4e47c6afa8928e9df2d036aea37
Vanessa Vasquez <Vanealeja1102@gmail.com>
2025-08-26 22:42:25 -0500

    Agregar description

--- src/dtos/product.dto.ts
+++ src/dtos/product.dto.ts
@@ -5,7 +5,9 @@ export class CreateProductDto {
   @IsString() @IsNotEmpty() @Length(2, 60) sku!: string;
   @IsNumber() @Min(0) price!: number;
   @IsOptional() @IsBoolean() active?: boolean;
+  @IsOptional() @IsString() @Length(2, 255) description?: string; // <-- agregado
 }
+
 
 export class UpdateProductDto {
   @IsOptional() @IsString() @Length(2, 120) name?: string;
