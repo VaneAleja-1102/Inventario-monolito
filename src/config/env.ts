f5d6569152f82d2471d7416275477b5549cb8333
Vanessa Vasquez <Vanealeja1102@gmail.com>
2025-08-26 23:08:50 -0500

    Arreglar env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,10 +1,10 @@
 export const env = {
   db: {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'dpg-d2n66en5r7bs73f5nnog-a',
     port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USER || 'user_java',
-    password: process.env.DB_PASS || '1523',
-    name: process.env.DB_NAME || 'inventario_db',
+    username: process.env.DB_USER || 'vanessa_sql_user',
+    password: process.env.DB_PASS || 'HywMnL4nthzGCyvBtiHohDGwN6L0Atqk',
+    name: process.env.DB_NAME || 'vanessa_sql',
     ssl: process.env.DB_SSL === 'true' ? true : false,
   },
   nodeEnv: process.env.NODE_ENV || 'development',
